0a72594470992a85b170aa603460c970c593e8fa
Amir Khuchraev <hooray.amir@gmail.com>
2025-03-08 11:17:25 +0200

    рефакторинг кода, добавил интерфейс как контракт по принципам архитектуры SOLID

--- src/main/java/amir/khuchraev/managers/RegistrationManagerInWindowsRegistry.java
+++ src/main/java/amir/khuchraev/managers/RegistrationManagerInWindowsRegistry.java
@@ -1,34 +1,13 @@
 package amir.khuchraev.managers;
 
-import com.sun.jna.platform.win32.Advapi32Util;
-import com.sun.jna.platform.win32.WinReg;
+public interface RegistrationManagerInWindowsRegistry {
 
-import static amir.khuchraev.Const.*;
-
-public class RegistrationManagerInWindowsRegistry {
-
-    public boolean programRegistrationInWindowsRegistry(
+    boolean programRegistrationInWindowsRegistry(
             String appKeyName,
             String displayName,
             String displayVersion,
             String publisher,
             String installLocation,
             String displayIcon,
-            String uninstallString
-    ) {
-        String fullAppKeyPath = UNINSTALL_PATH + "\\" + appKeyName;
-        boolean keyIsExists = false;
-        if (Advapi32Util.registryKeyExists(WinReg.HKEY_CURRENT_USER, fullAppKeyPath)) {
-            keyIsExists = true;
-        } else {
-            Advapi32Util.registryCreateKey(WinReg.HKEY_CURRENT_USER, UNINSTALL_PATH, appKeyName);
-        }
-        Advapi32Util.registrySetStringValue(WinReg.HKEY_CURRENT_USER, fullAppKeyPath, DISPLAY_NAME, displayName);
-        Advapi32Util.registrySetStringValue(WinReg.HKEY_CURRENT_USER, fullAppKeyPath, DISPLAY_VERSION, displayVersion);
-        Advapi32Util.registrySetStringValue(WinReg.HKEY_CURRENT_USER, fullAppKeyPath, PUBLISHER, publisher);
-        Advapi32Util.registrySetStringValue(WinReg.HKEY_CURRENT_USER, fullAppKeyPath, INSTALL_LOCATION, installLocation);
-        Advapi32Util.registrySetStringValue(WinReg.HKEY_CURRENT_USER, fullAppKeyPath, DISPLAY_ICON, displayIcon);
-        Advapi32Util.registrySetStringValue(WinReg.HKEY_CURRENT_USER, fullAppKeyPath, UNINSTALL_STRING, uninstallString);
-        return keyIsExists;
-    }
+            String uninstallString);
 }
---
+++ src/main/java/amir/khuchraev/managers/impl/RegistrationManagerInWindowsRegistryImpl.java
@@ -0,0 +1,36 @@
+package amir.khuchraev.managers.impl;
+
+import amir.khuchraev.managers.RegistrationManagerInWindowsRegistry;
+import com.sun.jna.platform.win32.Advapi32Util;
+import com.sun.jna.platform.win32.WinReg;
+
+import static amir.khuchraev.Const.*;
+
+public class RegistrationManagerInWindowsRegistryImpl implements RegistrationManagerInWindowsRegistry {
+
+    @Override
+    public boolean programRegistrationInWindowsRegistry(
+            String appKeyName,
+            String displayName,
+            String displayVersion,
+            String publisher,
+            String installLocation,
+            String displayIcon,
+            String uninstallString
+    ) {
+        String fullAppKeyPath = UNINSTALL_PATH + "\\" + appKeyName;
+        boolean keyIsExists = false;
+        if (Advapi32Util.registryKeyExists(WinReg.HKEY_CURRENT_USER, fullAppKeyPath)) {
+            keyIsExists = true;
+        } else {
+            Advapi32Util.registryCreateKey(WinReg.HKEY_CURRENT_USER, UNINSTALL_PATH, appKeyName);
+        }
+        Advapi32Util.registrySetStringValue(WinReg.HKEY_CURRENT_USER, fullAppKeyPath, DISPLAY_NAME, displayName);
+        Advapi32Util.registrySetStringValue(WinReg.HKEY_CURRENT_USER, fullAppKeyPath, DISPLAY_VERSION, displayVersion);
+        Advapi32Util.registrySetStringValue(WinReg.HKEY_CURRENT_USER, fullAppKeyPath, PUBLISHER, publisher);
+        Advapi32Util.registrySetStringValue(WinReg.HKEY_CURRENT_USER, fullAppKeyPath, INSTALL_LOCATION, installLocation);
+        Advapi32Util.registrySetStringValue(WinReg.HKEY_CURRENT_USER, fullAppKeyPath, DISPLAY_ICON, displayIcon);
+        Advapi32Util.registrySetStringValue(WinReg.HKEY_CURRENT_USER, fullAppKeyPath, UNINSTALL_STRING, uninstallString);
+        return keyIsExists;
+    }
+}
